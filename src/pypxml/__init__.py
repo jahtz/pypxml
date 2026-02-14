98092ff0f71373f7b4a9eb5bb9e307c6b71ce911
jahtz <jahtz.dev@proton.me>
2026-02-14 22:30:37 +0100

    move version to init file

--- src/pypxml/__init__.py
+++ src/pypxml/__init__.py
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
-from .__version__ import __version__
-
 from .pagexml import PageXML
 from .pageelement import PageElement
 from .pagetypes import PageType
 from .pageschema import PageSchema
 from .pageutil import PageUtil
+
+__version__ = '5.0.0'
--- src/pypxml/__version__.py
+++
@@ -1,2 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-__version__ = '5.0.0'
